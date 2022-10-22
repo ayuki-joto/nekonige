2356b1d52784d0a3cb4081c6162289c4858626dd
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-10-22 16:03:44 +0900

    feat: add loading

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,15 +1,25 @@
-import { FC } from 'react'
-import { Box, Center, Container } from '@chakra-ui/react'
+import { FC, useState } from 'react'
+import { Box, Center, Container, Spinner } from '@chakra-ui/react'
 
 const MapIndex: FC = () => {
+  const [loading, setLoading] = useState(true)
+
+  function hideSpinner() {
+    setLoading(false)
+  }
+
   return (
     <>
       <Container maxW={{ md: '80%', sm: '90%' }} height={'80vh'}>
         <Box textAlign={'center'}>
           <h1>ねこにげ(仮)マップ</h1>
         </Box>
-        <Center height={'100%'}>
+        <Center height={'100%'} display={loading ? 'flex' : 'none'}>
+          <Spinner />
+        </Center>
+        <Center height={'100%'} display={loading ? 'none' : 'flex'}>
           <iframe
+            onLoad={hideSpinner}
             src="https://www.google.com/maps/d/embed?mid=1pO9OONWCtfgm9GFemvfmA6xwg-PQPgg&ehbc=2E312F"
             width="80%"
             height="100%"
