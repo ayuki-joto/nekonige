b6c63c6bbcf1206b94125018ae16f30c448ea332
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2022-11-26 13:56:22 +0900

    feat: add article index

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,10 +1,44 @@
 import { FC } from 'react'
 import Header from 'component/Header'
+import CustomHeading from 'component/Heading/CustomHeading'
+import { Box, Button, Container, Grid } from '@chakra-ui/react'
+import Card from 'component/Card'
 
 const ArticleIndex: FC = () => {
   return (
     <>
       <Header />
+      <Container maxW={'8xl'} mb={20} mt={20}>
+        <CustomHeading imageSize={'54'}>
+          <Box fontSize={'3rem'}>避難の準備とポイント</Box>
+        </CustomHeading>
+        <Grid
+          templateColumns="repeat(3, 1fr)"
+          gap={6}
+          textAlign={'center'}
+          mt={20}
+        >
+          <Card
+            src={'/images/cat.jpg'}
+            width={1920}
+            height={1080}
+            alt={'飼い主の命にもかかわる　ペットの避難対策'}
+            link={'/articles/1'}
+          />
+          <Card
+            src={'/images/dummy.png'}
+            width={1920}
+            height={1080}
+            alt={'texttexttexttexttexttexttexttexttexttexttext'}
+          ></Card>
+          <Card
+            src={'/images/dummy.png'}
+            width={1920}
+            height={1080}
+            alt={'texttexttexttexttexttexttexttexttexttexttext'}
+          ></Card>
+        </Grid>
+      </Container>
     </>
   )
 }
