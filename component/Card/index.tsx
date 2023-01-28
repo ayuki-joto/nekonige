1e797be9464c95681771b219bbbb2084d898534f
ayuki_j <19406594+ayuki-joto@users.noreply.github.com>
2023-01-28 15:09:08 +0900

    fix: image size

--- component/Card/index.tsx
+++ component/Card/index.tsx
@@ -6,9 +6,11 @@ import { GridItem } from '@chakra-ui/react'
 
 const Card: FC<{
   src: string
+  width: number
+  height: number
   alt: string
   link?: string
-}> = ({ src, alt, link }) => {
+}> = ({ src, width, height, alt, link }) => {
   return (
     <GridItem
       colSpan={{ md: 1, base: 3 }}
@@ -18,9 +20,10 @@ const Card: FC<{
     >
       <Image
         src={src ? src : '/images/nekonige.png'}
+        width={width}
+        height={height}
         alt={alt}
-        layout="fill"
-        objectFit="contain"
+        objectFit="cover"
       />
       <div className={scss.content}>{alt}</div>
     </GridItem>
